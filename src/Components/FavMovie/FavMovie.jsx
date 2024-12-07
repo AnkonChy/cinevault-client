5d8e4936d90e21c0cf175a19f1a3ba9bc9e1c9f6
Ankon Chowdhury <ankonchy1@gmail.com>
2024-12-07 13:40:53 +0600

    delete favourite movie functionality done

--- src/Components/FavMovie/FavMovie.jsx
+++ src/Components/FavMovie/FavMovie.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import { CiStar } from "react-icons/ci";
 import { FaStar } from "react-icons/fa";
 import Rating from "react-rating";
-import { Link } from "react-router-dom";
 
-const FavMovie = ({ favMovie }) => {
+const FavMovie = ({ favMovie, favMovies, setFavMovies }) => {
   const { _id, poster, title, genre, duration, year, rating } = favMovie;
+
+  const handleDelete = (id) => {
+    fetch(`http://localhost:4000/favMovie/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        const newData = favMovies.filter((fMovie) => id !== fMovie._id);
+        setFavMovies(newData);
+      });
+  };
   return (
     <div className="card card-compact bg-base-100 shadow-xl rounded-none">
       <figure>
@@ -30,11 +40,12 @@ const FavMovie = ({ favMovie }) => {
         </div>
 
         <div className="card-actions justify-center mt-4">
-          <Link to={`/movie/${_id}`}>
-            <button className="text-lg py-2 px-6 rounded-full bg-gray-800 text-white border hover:bg-blue-900">
-              See Details
-            </button>
-          </Link>
+          <button
+            onClick={() => handleDelete(_id)}
+            className="text-lg py-2 px-6 rounded-full bg-gray-800 text-white border hover:bg-blue-900"
+          >
+            Delete Favourite
+          </button>
         </div>
       </div>
     </div>
--- src/Components/FavMovies/FavMovies.jsx
+++ src/Components/FavMovies/FavMovies.jsx
@@ -22,7 +22,12 @@ const Favourite = () => {
       </h1>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
         {favMovies.map((favMovie) => (
-          <FavMovie key={favMovie._id} favMovie={favMovie}></FavMovie>
+          <FavMovie
+            key={favMovie._id}
+            favMovie={favMovie}
+            favMovies={favMovies}
+            setFavMovies={setFavMovies}
+          ></FavMovie>
         ))}
       </div>
     </div>
