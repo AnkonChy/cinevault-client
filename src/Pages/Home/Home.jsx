dbdd923908ce9334ab4c7695ad9521557e01a939
Ankon Chowdhury <ankonchy1@gmail.com>
2024-12-08 11:45:20 +0600

    animate.css and aos package applied successfully

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "../../Components/Slider/Slider";
 import { NavLink, useLoaderData } from "react-router-dom";
 import FeaturedMovie from "../../Components/FeaturedMovie/FeaturedMovie";
 import LatestMovies from "../../Components/LatestMovies/LatestMovies";
 import BanglaMovies from "../../Components/BanglaMovies/BanglaMovies";
+import "aos/dist/aos.css";
+import Aos from "aos";
+import "animate.css";
 
 const Home = () => {
+  useEffect(() => {
+    Aos.init();
+  }, []);
   const { sortMoviesData, latestMoviesData, banglaMoviesData } =
     useLoaderData();
 
@@ -16,7 +22,10 @@ const Home = () => {
         <h1 className=" animate__animated animate__fadeInLeft text-3xl md:text-4xl lg:text-5xl font-bold">
           Featured Movies
         </h1>
-        <div className=" grid md:grid-cols-2 lg:grid-cols-3 gap-6  my-10">
+        <div
+          className=" grid md:grid-cols-2 lg:grid-cols-3 gap-6  my-10"
+          data-aos="fade-right"
+        >
           {sortMoviesData.map((movie) => (
             <FeaturedMovie key={movie._id} movie={movie}></FeaturedMovie>
           ))}
@@ -31,7 +40,11 @@ const Home = () => {
         <h1 className=" animate__animated animate__fadeInLeft text-3xl md:text-4xl lg:text-5xl font-bold">
           Latest Movies
         </h1>
-        <div className=" grid md:grid-cols-2 lg:grid-cols-3 gap-6  my-10">
+        <div
+          className=" grid md:grid-cols-2 lg:grid-cols-3 gap-6  my-10"
+          data-aos="fade-left"
+          data-aos-duration="2000"
+        >
           {latestMoviesData.slice(0, 3).map((movie) => (
             <LatestMovies key={movie._id} movie={movie}></LatestMovies>
           ))}
@@ -41,7 +54,10 @@ const Home = () => {
         <h1 className=" animate__animated animate__fadeInLeft text-3xl md:text-4xl lg:text-5xl font-bold">
           Bangla Movies
         </h1>
-        <div className=" grid md:grid-cols-2 lg:grid-cols-3 gap-6  my-10">
+        <div
+          className=" grid md:grid-cols-2 lg:grid-cols-3 gap-6  my-10"
+          data-aos="fade-down-right"
+        >
           {banglaMoviesData.slice(0, 3).map((movie) => (
             <BanglaMovies key={movie._id} movie={movie}></BanglaMovies>
           ))}
